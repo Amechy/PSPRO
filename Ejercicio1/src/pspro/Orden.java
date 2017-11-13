2c6eaf0d345949611d8ee210f4d7475a851f8a84
Amechy <alejandromechinegamarra@gmail.com>
2017-11-13 14:32:02 +0100

    ejercicio1

--- Ejercicio1/src/pspro/Orden.java
+++ Ejercicio1/src/pspro/Orden.java
@@ -1,5 +1,7 @@
 package pspro;
 
 public class Orden {
-
+public static void main(String[] args) {
+	
+}
 }
--- Ejercicio1/src/pspro/hilo.java
+++ Ejercicio1/src/pspro/hilo.java
@@ -1,5 +1,11 @@
 package pspro;
 
 public class hilo extends Thread {
-
+	@Override
+	public void run() {
+		Saluda();
+	}
+	public void Saluda() {
+			"Hol"
+		}
 }
